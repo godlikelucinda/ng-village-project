1ccccdf6c5ae6d640d173271e5d6a966850366df
Lucinda <34999817+godlikelucinda@users.noreply.github.com>
2018-12-19 15:51:43 +0100

    Application und Tests leicht angepasst.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-village-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-village-project');
+    expect(app.title).toEqual('village');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-village-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to our village!');
   });
 });
